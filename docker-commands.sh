42b375fb919c53b986941e63e31a670da82e4235
irfanhanif <ih.irfanhanif@gmail.com>
2020-08-02 12:02:03 +0700

    Adjust typo

--- docker-commands.sh
+++ docker-commands.sh
@@ -1,7 +1,7 @@
 # build docker image by always remove intermediate containers
 docker build --force-rm -t image_name:tag .
 
-# run container, router port 9090 to 9090, and remove the container when it exits
+# run container, route port 9090 to 9090, and remove the container when it exits
 docker run --rm -p 9090:9090 --name image_name container_name
 
 # cleaning all unused docker resources
